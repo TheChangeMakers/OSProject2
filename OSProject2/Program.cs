3133728cf3b29ea9489fa3ad24d5bbdf0abdf706
MR <n00943512@ospreys.unf.edu>
2017-10-06 19:45:11 -0400

    added Job, Exec, File, TimeQuantum classes - MR

--- OSProject2/Program.cs
+++ OSProject2/Program.cs
@@ -10,6 +10,35 @@ namespace OSProject2
     {
         static void Main(string[] args)
         {
+            // p2 is the name of the executable
+            string p2;
+
+            // fn is the name of the file that contains a sequence of jobs to use for the simulation
+            string fn;
+
+            // tq is the Time Quantum value to use for the Round Robin algorithm
+            int tq;
+
+
         }
     }
+
+    class Job
+    {
+        public int ArrivalTime { get; set; }
+        public int ReqTimeCycles { get; set; }
+    }
+    class Executable
+    {
+        public string ExecutableName { get; set; }
+    }
+    class File
+    {
+        public string FileName { get; set; }
+    }
+    class TimeQuantum
+    {
+        public int TQValue { get; set; }
+    }
+
 }
