e7be5d1cc922122bbdf7419b00c0d6bdd1e4ed48
MR <n00943512@ospreys.unf.edu>
2017-10-07 14:43:20 -0400

    Red Robin Method - MR

--- OSProject2/Program.cs
+++ OSProject2/Program.cs
@@ -42,14 +42,102 @@ namespace OSProject2
     public class RedRobin
     {
         public JobList JobList;
-        public Queue JobQueue;
-        public int TimeQuantum { get; set; }
+        List<Job> JobQueue;
 
         public RedRobin(JobList jobList)
         {
             JobList = jobList;
         }
+
+        public double PerformRedRobin(int timeQuantum)
+        {
+            int currentTimeQuantumValue;
+
+            // Stores completed jobs
+            List<Job> completedJobs = new List<Job>();
+
+            // Stores incomplete jobs ready for processing
+            JobQueue = new List<Job>();
+
+            Job currentJob = null;
+
+            // Start at time 0 and go through total job process time
+            for (int currentTime = 0; currentTime <= JobList.GetTotalJobListProcessTime(); currentTime++)
+            {
+                // add new jobs to queue
+                for (int i = 0; i < JobList.GetJobCount(); i++)
+                {
+                    // if job is not in Q and job arrival time = current time
+                    if (!JobList.ListOfJobs[i].AddedToQueue && JobList.ListOfJobs[i].ArrivalTime == currentTime)
+                    {
+                        // add job to Q 
+                        JobQueue.Add(JobList.ListOfJobs[i]);
+                        JobList.ListOfJobs[i].AddedToQueue = true;
+                    }
+                }
+
+                // only executes once, at beginning 
+                if (currentJob == null)
+                {
+                    // assign 1st job in Q to current job
+                    currentJob = JobQueue[0];
+
+                    // remove job from Q
+                    JobQueue.RemoveAt(0);
+
+                    // reset time quantum
+                    currentTimeQuantumValue = timeQuantum;
+                }
+
+                // test if current job has completed all cycles 
+                if (currentJob.CyclesRemaining == 0)
+                {
+                    //set current job completion time 
+                    currentJob.CompletionTime = currentTime;
+
+                    // add job to completed job list
+                    completedJobs.Add(currentJob);
+
+                    // test if jobs remaining in Q
+                    if (JobQueue[0] != null)
+                    {
+                        // assign first job in Q to current job
+                        currentJob = JobQueue[0];
+
+                        // remove job from Q
+                        JobQueue.RemoveAt(0);
+
+                        // reset time quantum
+                        currentTimeQuantumValue = timeQuantum;
+                    }
+                }
+                else // now we know current job has cycles remaining 
+                {
+                    // if time quantum has expired
+                    if (currentTimeQuantumValue == 0)
+                    {
+                        // find index of new shortest job in Q
+                        int indexOfNewShortestJob = FindShortestJob();
+
+                        // add current job to rear of Q
+                        JobQueue.Add(currentJob);
+
+                        // assign new shortest job to currentJob
+                        currentJob = JobQueue[indexOfNewShortestJob];
+
+                        // remove shortest job from Q
+                        JobQueue.RemoveAt(indexOfNewShortestJob);
+                    }
+                }
+
+                // decrement currentJob.CycleRemaining
+                currentJob.CyclesRemaining -= 1;
+            }
+            // return turn around time
+            return JobList.ComputeTurnAroundTime(completedJobs);
+        }
     }
+}
 
     public class Job
     {
