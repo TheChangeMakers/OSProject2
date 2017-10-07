56cdafc80664b1437b045ab08e95960b8d0a7a36
Katie Grubbs <katie.grubbs@gmail.com>
2017-10-07 16:20:23 -0400

    Put RoundRobin class in separate file -KG

---
+++ OSProject2/RoundRobin.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSProject2
+{
+    public class RoundRobin
+    {
+        public JobList JobList;
+        List<Job> JobQueue;
+
+        public RoundRobin(JobList jobList)
+        {
+            JobList = jobList;
+        }
+
+        public void PerformRoundRobin(int timeQuantum)
+        {
+            int currentTimeQuantumValue = timeQuantum;
+
+            // Stores completed jobs
+            List<Job> completedJobs = new List<Job>();
+
+            // Stores incomplete jobs ready for processing
+            JobQueue = new List<Job>();
+
+            Job currentJob = null;
+
+            // Start at time 0 and go through total job process time
+            for (int currentTime = 0; currentTime <= JobList.GetTotalJobListProcessTime(); currentTime++)
+            {
+                // add new jobs to queue
+                for (int i = 0; i < JobList.GetJobCount(); i++)
+                {
+                    // if job is not in Q and job arrival time = current time
+                    if (!JobList.ListOfJobs[i].AddedToQueue && JobList.ListOfJobs[i].ArrivalTime == currentTime)
+                    {
+                        // add job to Q 
+                        JobQueue.Add(JobList.ListOfJobs[i]);
+                        JobList.ListOfJobs[i].AddedToQueue = true;
+                    }
+                }
+
+                // only executes once, at beginning 
+                if (currentJob == null)
+                {
+                    // assign 1st job in Q to current job
+                    currentJob = JobQueue[0];
+
+                    // remove job from Q
+                    JobQueue.RemoveAt(0);
+
+                    // reset time quantum
+                    currentTimeQuantumValue = timeQuantum;
+                }
+
+                // test if current job has completed all cycles 
+                if (currentJob.CyclesRemaining == 0)
+                {
+                    //set current job completion time 
+                    currentJob.CompletionTime = currentTime;
+
+                    // add job to completed job list
+                    completedJobs.Add(currentJob);
+
+                    // test if jobs remaining in Q
+                    if (JobQueue[0] != null)
+                    {
+                        // assign first job in Q to current job
+                        currentJob = JobQueue[0];
+
+                        // remove job from Q
+                        JobQueue.RemoveAt(0);
+
+                        // reset time quantum
+                        currentTimeQuantumValue = timeQuantum;
+                    }
+                }
+                else // current job has cycles remaining 
+                {
+                    // if time quantum has expired
+                    if (currentTimeQuantumValue == 0 && JobQueue[0] != null)
+                    {
+                        // add current job to rear of Q
+                        JobQueue.Add(currentJob);
+
+                        // assign 1st job in Q to current job
+                        currentJob = JobQueue[0];
+
+                        // reset time quantum
+                        currentTimeQuantumValue = timeQuantum;
+                    }
+                }
+
+                // decrement time quantum
+                currentTimeQuantumValue -= 1;
+
+                // decrement currentJob.CycleRemaining
+                currentJob.CyclesRemaining -= 1;
+            }
+
+            // compute turnaround times
+            Console.WriteLine("Round Robin Information:");
+            JobList.ComputeTurnaroundTimes(completedJobs);
+        }
+    }
+}
